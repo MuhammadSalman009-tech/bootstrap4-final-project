067f067d8df9c2dd50bab16a0a02365816389348
muhammad salman <salmansumra009@gmail.com>
2020-05-22 07:00:03 -0700

    bootstrap4 final project

---
+++ owlcarousel.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        }
+    }
+})
